3a73bc3d354cff1ed5c5a1340a1cca946ee9ba34
Ixedeq <Alex102602@gmail.com>
2025-09-04 03:49:37 -0500

    sidebar#5

--- src/components/components/MenuBox.jsx
+++ src/components/components/MenuBox.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Sidebar from "./Sidebar"; // your sidebar component
 
 export default function MenuBox() {
   const [isOpen, setIsOpen] = useState(false);
