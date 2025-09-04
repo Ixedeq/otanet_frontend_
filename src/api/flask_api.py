46d21028b3724a4872c5ea43bac273577d4b3d4f
NyZaireyus Harrison <53630482+xXKamiSureyiaXx@users.noreply.github.com>
2025-09-03 17:58:42 -0700

    Changed prefix to use base key

--- src/api/flask_api.py
+++ src/api/flask_api.py
@@ -160,7 +160,7 @@ def get_pages():
     base_key = f"{parsed_title}/{chapter}" 
     keys = []
 
-    for obj in bucket.objects.filter(Prefix=key):
+    for obj in bucket.objects.filter(Prefix=base_key):
         obj = obj.key.rsplit('/')
         keys.append(obj[2])
     sorted_keys = sorted(keys, key=get_first_number)
