e6330774fc5c4bc3abaf93398cd6f694b863fb39
Ixedeq <99107626+Ixedeq@users.noreply.github.com>
2025-09-06 03:58:24 -0500

    fixing fullscreen 6.5

--- src/components/components/ChapterImg.jsx
+++ src/components/components/ChapterImg.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 
 export default function ChapterImg({ src, alt, onOpenFullscreen, index }) {
-  const handleTap = (e) => {
-    e.preventDefault();
-    onOpenFullscreen(index); // tell parent which page
-  };
 
   const handleDoubleClick = (e) => {
     e.preventDefault();
@@ -14,7 +10,6 @@ export default function ChapterImg({ src, alt, onOpenFullscreen, index }) {
   return (
     <div
       className="chapter-img-wrapper"
-      onTouchEnd={handleTap}
       onDoubleClick={handleDoubleClick}
     >
       <img src={src} alt={alt} className="chapter-img" draggable={false} />
