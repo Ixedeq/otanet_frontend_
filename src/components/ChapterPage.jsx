66ddd883ba48ac8af24af2b3cff1b6b23eae486a
Ixedeq <99107626+Ixedeq@users.noreply.github.com>
2025-09-07 01:24:24 -0500

    minor change

--- src/components/ChapterPage.jsx
+++ src/components/ChapterPage.jsx
@@ -8,7 +8,7 @@ import "../css/ChapterPage.css";
 export default function ChapterPage() {
   const { slug, chapter } = useParams();
   const chapterKey = chapter.replace("-", "_");
-  const chapterNumberStr = chapter.split [1];
+  const chapterNumberStr = chapter.split[1];
 
   const [mangaTitle, setMangaTitle] = useState("");
   const [pages, setPages] = useState([]);
