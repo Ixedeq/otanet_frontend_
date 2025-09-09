bc41125fa6ee6526b199ab6c4413263d0569e6be
Ixedeq <Alex102602@gmail.com>
2025-09-09 01:00:48 -0500

    header fix mobile test

--- src/components/components/MenuBox.jsx
+++ src/components/components/MenuBox.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function MenuBox() {
   return (
-    <div className="MenuBox">
-      <button className="searchButton">☰</button>
+    <div className="searchBox">
+      <button className="MenuBox">☰</button>
     </div>
   );
 }
